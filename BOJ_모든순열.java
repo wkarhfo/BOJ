966e63676eb1b425a272c9b60d61d60bc55e2e1f
wkarhfo <50615511+wkarhfo@users.noreply.github.com>
2020-03-11 18:52:42 +0900

    Create BOJ_모든순열.java

---
+++ BOJ_모든순열.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class BOJ_모든순열 {
+	static int[] result;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer(br.readLine());
+		int N=Integer.parseInt(st.nextToken());
+		result=new int[N];
+		for(int i=1;i<=N;i++) {
+			result[i-1]=i;
+			System.out.print(result[i-1]+" ");
+		}
+		System.out.println();
+		while(NextPermitation()) {
+			for(int i=0;i<result.length;i++) {
+				System.out.print(result[i]+" ");
+			}
+			System.out.println();
+		}
+	}
+	static boolean NextPermitation() {
+		int tmp=result.length-1;
+		while(tmp>0&&result[tmp-1]>=result[tmp]) {
+			tmp--;
+		}
+		if(tmp<=0) 
+			return false;
+		int temp=result.length-1;
+		while(result[tmp-1]>=result[temp]) {
+			temp--;
+		}
+		
+		int swap=result[tmp-1];
+		result[tmp-1]=result[temp];
+		result[temp]=swap;
+		
+		ArrayList<Integer> list=new ArrayList<>();
+		for(int i=tmp;i<result.length;i++) {
+			list.add(result[i]);
+		}
+		Collections.sort(list);
+		for(int i=tmp;i<result.length;i++) {
+			result[i]=list.remove(0);
+		}
+		return true;
+	}
+
+}
