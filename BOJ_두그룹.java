b878d9d2d9838c395d5e683699e0ff7032c86061
wkarhfo <50615511+wkarhfo@users.noreply.github.com>
2020-04-20 23:46:51 +0900

    Add files via upload

---
+++ BOJ_두그룹.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_두그룹 {
+	static int A, B, C;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		A=Integer.parseInt(st.nextToken());
+		B=Integer.parseInt(st.nextToken());
+		C=Integer.parseInt(st.nextToken());
+		
+	}
+}
