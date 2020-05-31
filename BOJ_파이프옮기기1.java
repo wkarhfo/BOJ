c2f5ede6fc6d816629775b9b991990a648004ec1
wkarhfo <50615511+wkarhfo@users.noreply.github.com>
2020-06-01 01:15:41 +0900

    dfs+가지치기 문제

--- BOJ_파이프옮기기1.java
+++ BOJ_파이프옮기기1.java
@@ -4,61 +4,70 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class BOJ_파이프옮기기1 {
+	static int N;
 	static int[][] arr;
-	static int resultH;
-	static int resultY;
-	static int count = 0;
-	static int[] dh = { 0, 1, 1 }; //가로 세로 대각선
+	static int endh, endy;
+	static int[] dh = { 0, 1, 1 }; // 우, 하, 대각선
 	static int[] dy = { 1, 0, 1 };
+	static int count = 0;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = Integer.parseInt(br.readLine().trim());
-		arr = new int[size][size];
-		resultH = resultY = size - 1;
-		for (int i = 0; i < size; i++) {
+		N = Integer.parseInt(br.readLine().trim());
+		arr = new int[N][N];
+		for (int i = 0; i < N; i++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < size; j++) {
+			for (int j = 0; j < N; j++) {
 				arr[i][j] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		dfs(0, 1, 0);
+		endh = endy = N - 1; // 도착점
+		// 2:우 3:하 4:우하
+		dfs(0, 1, 2);
 		System.out.println(count);
 	}
 
-	private static void dfs(int h, int y, int type) {
-		if(h==resultH&&y==resultY) {
+	static void dfs(int h, int y, int type) {
+		if (h == endh && y == endy) {
 			count++;
 			return;
 		}
-		for(int k=0;k<3;k++) {
-			if(type==0&&k==1) // 가로일 경우 세로로 x
+
+		for (int k = 0; k < 3; k++) {
+
+			if (type == 2 && k == 1) { // 가로 ->하는 해당 안됨
 				continue;
-			else if(type==1&&k==0) //세로일 경우 가로로 x
+			} else if (type == 3 && k == 0) { // 세로-> 우는 해당안됨
 				continue;
-			if(isPromising(h,y,k)) {
-				int ah=h+dh[k];
-				int ay=y+dy[k];
-				dfs(ah,ay,k);
+			} else if (type == 4) { // 대각선
+
 			}
-			
+
+			if (ischeck(h, y, k)) {
+				int ah = h + dh[k];
+				int ay = y + dy[k];
+				dfs(ah, ay, k + 2);
+			}
+
 		}
-		
 	}
 
-	private static boolean isPromising(int h, int y, int way) {
-		int ah=h+dh[way];
-		int ay=y+dy[way];
-		if(ah<0||ah>=arr.length||ay<0||ay>=arr[0].length)
+	static boolean ischeck(int h, int y, int k) { // 1이 있는지 없는지 체크하는 함수
+		int ah = h + dh[k];
+		int ay = y + dy[k];
+		if (ah < 0 || ah >= arr.length || ay < 0 || ay >= arr[0].length)
 			return false;
-		if(way==2) {
-			if(arr[ah][ay]==1||arr[ah-1][ay]==1||arr[ah][ay-1]==1)
+		if (k == 0) { // 가로 방향으로 나아갈때
+			if (arr[ah][ay] == 1)
 				return false;
-		}else {
-			if(arr[ah][ay]==1)
+		} else if (k == 1) { // 세로 일때
+			if (arr[ah][ay] == 1)
+				return false;
+		} else if (k == 2) { // 대각선 방향으로 나아갈때
+			if (arr[ah][ay] == 1 || arr[ah - 1][ay] == 1 || arr[ah][ay - 1] == 1)
 				return false;
 		}
 		return true;
 	}
+
 }
